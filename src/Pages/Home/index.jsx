1da231d0f119b5f3ee6b9565d491f305aed92de4
EAMR22 <elvis1042.rodriguez@gmail.com>
2024-03-28 12:21:10 -0600

    Consumiendo FakeStore API para rellenar las Cards

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,12 +1,27 @@
+import { useState, useEffect } from 'react' // con useState nos permite guardar la informacion
+// Con useEffect nos permite hacer consumo de API
 import Layout from '../../Components/Layout'
 import Card from '../../Components/Card'
 
 function Home() {
+    const [items, setItems] = useState(null)// setItems nos ayuda a almacenar la informacion que se necesita y lo almacena en items.
+
+    useEffect(() => {
+      fetch('https://fakestoreapi.com/products')
+        .then(response => response.json())
+        .then(data => setItems(data))
+    }, [])
     return (
       <>
         <Layout>
           Home
-          <Card />
+          <div className='grid gap-4 grid-cols-4 w-full max-w-screen-lg'>
+            {
+              items?.map(item => ( // Por cada elemento que esta en items se muestra una Card.
+                <Card key={item.id} data={item} /> // data envia la informacion a la Card
+              ))
+            }
+          </div>
         </Layout>
       </>
     )
