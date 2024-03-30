f8b3e46a4504b552a59b1a4ea50d9d67cc2ee01f
EAMR22 <elvis1042.rodriguez@gmail.com>
2024-03-29 21:26:59 -0600

    Checkout de productos en el carrito

--- src/Pages/MyOrder/index.jsx
+++ src/Pages/MyOrder/index.jsx
@@ -1,13 +1,30 @@
+import { useContext} from 'react'
+import { ShoppingCartContext } from '../../Context'
 import Layout from "../../Components/Layout"
+import OrderCard from '../../Components/OrderCard'
 
 function MyOrder() {
+    const context = useContext(ShoppingCartContext)
+
     return (
-      <>
-        <Layout>
-          MyOrder
-        </Layout>
-      </>
-    )
+        <>
+            <Layout>
+                MyOrder
+                <div className='flex flex-col w-80'>
+                    {
+                        context.order?.slice(-1)[0].products.map(product => ( // Muestra la ultima orden que se tomo, y como es un array se toma en la posicion 0.
+                            <OrderCard
+                                key={product.id}
+                                id={product.id}
+                                title={product.title}
+                                imageUrl={product.image}
+                                price={product.price} />
+                        ))
+                    }
+                </div>
+            </Layout>
+          </>
+      )
   }
   
 export default MyOrder
